e7af4f92848d73f18c3511e4ffc8cf4876a3622d
unknown <ososanyaola8@gmail.com>
2026-06-19 06:46:15 +0100

    updated code

---
+++ app/checkout/page.jsx
@@ -0,0 +1,8 @@
+export default function CheckoutPage() {
+  return (
+    <div style={{ padding: "4rem 1.5rem", textAlign: "center" }}>
+      <h1>Checkout</h1>
+      <p>This page is coming soon.</p>
+    </div>
+  );
+}
